befcf2a8990ee635511d3d66fb6cc900d711876d
Matheus Pontes <pontes.matheus.14@gmail.com>
2023-06-08 16:33:38 -0300

    Finalizacao do bd

--- aceleraClube/src/Banco/associadosBanco.java
+++ aceleraClube/src/Banco/associadosBanco.java
@@ -56,7 +56,7 @@ public class associadosBanco {
     } 
 
 
-public boolean atualizarAssociado(Associados associados, String nomeAlterar) 
+public boolean atualizarAssociado(Associados associados, String CPFAlterar) 
     {
         System.out.println("Atualizar Pessoa");
         
@@ -72,7 +72,7 @@ public boolean atualizarAssociado(Associados associados, String nomeAlterar)
                          "endereco='"+associados.getEndereco()+"', telefone='"+associados.getTelefone()+"'," +
                          "email='"+associados.getEmail()+"', cpf='"+associados.getCPF()+"'," +
                          "cep='"+associados.getCEP()+"', nascimento='"+associados.getNascimento()+"',"+
-                         "cidade='"+associados.getCidade()+"' WHERE nome='"+nomeAlterar+"';";
+                         "cidade='"+associados.getCidade()+"' WHERE nome='"+CPFAlterar+"';";
                     
             System.out.println("SQL: " + sql);
             stmt.executeUpdate(sql);
@@ -98,6 +98,150 @@ public boolean atualizarAssociado(Associados associados, String nomeAlterar)
             }
         }
     }
+
+ public String relatorioPessoas()
+    {
+    	String dados = "";
+    	
+    	connection = conectarBanco.getInstance().getConnection();
+        System.out.println("Conectado e preparando a listagem");
+        Statement stmt = null;
+        
+        try
+        {
+            stmt = connection.createStatement();
+            ResultSet res = stmt.executeQuery("SELECT * FROM dados");
+            
+            while (res.next())
+            {
+            	dados += "\n"+res.getString("Nome");
+				dados += "\n"+res.getString("Endereco");
+				dados += "\n"+res.getString("Telefone");
+				dados += "\n"+res.getString("Email");
+                                dados += "\n"+res.getString("CPF");
+                                dados += "\n"+res.getString("CEP");
+                                dados += "\n"+res.getString("Nascimento");
+                                dados += "\n"+res.getString("Cidade");
+				dados += "\n-----------------------------------------";				
+            }
+            
+             } 
+        catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+            return null;
+        }
+        finally
+        {
+          
+            try
+            {
+                stmt.close();
+                connection.close();
+            }
+            catch (SQLException e)
+            {
+                System.out.println("Erro ao desconectar" + e.getMessage());
+            }
+        }
+    	
+    	return dados;
+    }
+ 
+ public ArrayList<Associados> getListadosAssociados() 
+    {
+        ArrayList<Associados> listaAssociados = new ArrayList<Associados>();
+      
+        connection = conectarBanco.getInstance().getConnection();
+        System.out.println("Conectado e preparando a listagem");
+        Statement stmt = null;
+        
+        try
+        {
+            stmt = connection.createStatement();
+            ResultSet res = stmt.executeQuery("SELECT * FROM dados");
+            
+            while (res.next())
+            {
+                Associados associados = new Associados(res.getString("Nome"), res.getString("Endereco"), 
+                		                   res.getString("Telefone"),res.getString("Email"),
+                                                   res.getString("CPF"),res.getString("CEP"),
+                                                   res.getString("Nascimento"),res.getString("Cidade"));
+                listaAssociados.add(associados);
+            }
+            
+        } 
+        
+        catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+            return null;
+        }
+        finally
+        {
+          
+            try
+            {
+                stmt.close();
+                connection.close();
+            }
+            catch (SQLException e)
+            {
+                System.out.println("Erro ao desconectar" + e.getMessage());
+            }
+        }
+        return listaAssociados;
+    } 
+ 
+    public boolean excluirAssociado(Associados associados) 
+        {
+    	boolean status = true;
+    	
+        System.out.println("Excluir Associado");
+        
+        connection = conectarBanco.getInstance().getConnection();
+        System.out.println("Conectado e pronto para excluir");
+        Statement stmt = null;
+        
+        try 
+        {
+            stmt = connection.createStatement();
+            
+            String sql = "DELETE FROM dados WHERE CPF='"+associados.getCPF()+"'";
+            
+            System.out.println("SQL: " + sql);
+            
+            if(stmt.executeUpdate(sql)!=0)
+            {
+            	status = true;	
+            }
+            else
+            {
+            	status = false;
+            }
+            
+        } 
+        catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+            status = false;
+        }
+        finally 
+        {
+            
+            try
+            {
+                stmt.close();
+                connection.close();
+            } 
+            catch (SQLException e)
+            {
+                System.out.println("Erro ao desconectar" + e.getMessage());
+            }
+        }
+        return status;
+    }
+
         
         
     
